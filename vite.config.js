67b288327b69488a10300c3c96ac9b3af443675f
royertqm <royertgojo@gmail.com>
2026-06-29 10:36:22 -0600

    Configurar despliegue con GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import path from 'path';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: '/perfil-profesional/',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
